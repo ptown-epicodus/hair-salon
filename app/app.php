b1bc5ac182690118c63aeb3bde084117e79d9587
Patrick McGreevy <patrick7490@icloud.com>
2017-02-24 13:46:08 -0800

    Add routes for Client model

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@ date_default_timezone_set('America/Los_Angeles');
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Stylist.php';
+require_once __DIR__ . '/../src/Client.php';
 
 $app = new Silex\Application();
 
@@ -31,6 +32,22 @@ $app->post('/stylists', function() use ($app) {
     $new_stylist = new Stylist($name);
     $new_stylist->save();
     return $app->redirect('/stylists');
+});
+
+$app->get('/stylists/{id}', function($id) use ($app) {
+    $stylist = Stylist::find($id);
+    $clients = $stylist->getClients();
+    return $app['twig']->render('stylist.html.twig', [
+        'stylist' => $stylist,
+        'clients' => $clients
+    ]);
+});
+
+$app->post('/stylists/{id}/clients', function($id) use ($app) {
+    $name = $_POST['name'];
+    $new_client = new Client($name, $id);
+    $new_client->save();
+    return $app->redirect("/stylists/{$id}");
 });
 
 return $app;
